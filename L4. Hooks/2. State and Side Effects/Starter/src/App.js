74616373f406f325cbe0bd46736870163210a3ec
jacobmmm <jacob.42193@gmail.com>
2026-01-09 21:10:08 +1100

    Upto useEffect

--- L4. Hooks/2. State and Side Effects/Starter/src/App.js
+++ L4. Hooks/2. State and Side Effects/Starter/src/App.js
@@ -1,14 +1,46 @@
 import logo from "./logo.svg";
 import "./App.css";
+import {useState, useEffect} from "react"
 
-const value1 = Math.floor(Math.random() * 100);
+
+const App = () => {
+
+  const value1 = Math.floor(Math.random() * 100);
 const value2 = Math.floor(Math.random() * 100);
 const value3 = Math.floor(Math.random() * 100);
 const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
-const numQuestions = 0;
-const numCorrect = 0;
+let sum = 0;
+const [numQuestions,setNumQuestions] = useState(0)
+const [numCorrect,setNumCorrect] = useState(0)
 
-const App = () => {
+useEffect(() => {
+  
+    sum=value1+value2+value3
+    
+}
+,[])
+
+const checkTrue = () => {
+  console.log("CHecking For True")
+  console.log("Sum: ",sum)
+  if (sum===proposedAnswer)
+    setNumCorrect((p) => p+1)
+
+  setNumQuestions((p) => p+1)
+}
+
+const checkFalse = () => {
+  console.log("CHecking For False")
+  console.log("Sum: ",sum)
+  if (sum!==proposedAnswer)
+    setNumCorrect((p) => p+1)
+  setNumQuestions((p) => p+1)
+  
+}
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -20,8 +52,8 @@ const App = () => {
         <div className="equation">
           <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
         </div>
-        <button>True</button>
-        <button>False</button>
+        <button onClick={checkTrue}>True</button>
+        <button onClick={checkFalse}>False</button>
         <p className="text">
           Your Score: {numCorrect}/{numQuestions}
         </p>
